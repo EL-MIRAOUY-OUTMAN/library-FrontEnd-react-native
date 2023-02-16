61fd972cb05ac5d68ef2041d1c95854bee72fc22
unknown <fatima.otman2017@gmail.com>
2023-02-16 17:30:37 +0100

    complit review

--- src/layouts/BookCheckOutPage/BoockcCheckOutPage.tsx
+++ src/layouts/BookCheckOutPage/BoockcCheckOutPage.tsx
@@ -271,7 +271,7 @@ export const BookCheckoutPage = () => {
                             <h2>{book?.title}</h2>
                             <h5 className='text-primary'>{book?.author}</h5>
                             <p className='lead'>{book?.description}</p>
-                            <StarReview rating={4} size={32}/>
+                            <StarReview rating={2.5} size={32}/>
                         </div>
                     </div>
                     <p>hello</p>  </div>
